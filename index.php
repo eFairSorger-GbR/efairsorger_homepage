d8f0915995b7c50517bc2e0702db4dd12939d1bc
Kevin Liebholz <liebholz@crowddesk.de>
2020-12-18 17:55:40 +0100

    send to https

--- index.php
+++ index.php
@@ -1,1 +1,11 @@
-<?php include_once("index.html"); ?>
+<?php
+  if($_SERVER["HTTPS"] != "on")
+  {
+      header("Location: https://" . $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"]);
+      exit();
+  }
+  else
+  {
+    include_once("index.html");
+  }
+?>

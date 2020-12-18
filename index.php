57da09bf042bb4f2207cd29e70d8ccf0b19246fb
Kevin Liebholz <liebholz@crowddesk.de>
2020-12-18 17:59:55 +0100

    FIX: redirect ssl

--- index.php
+++ index.php
@@ -1,11 +1,4 @@
 <?php
-  if($_SERVER["HTTPS"] != "on")
-  {
-      header("Location: https://" . $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"]);
-      exit();
-  }
-  else
-  {
-    include_once("index.html");
-  }
+  header("Location: https://www.efairsorger.de", TRUE, 301);
+  exit();
 ?>

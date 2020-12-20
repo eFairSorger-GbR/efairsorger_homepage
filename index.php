d5a1b964612f84950dd57887847f7bb3143a470f
Kevin Liebholz <liebholz@crowddesk.de>
2020-12-20 12:34:29 +0100

    ssl

--- index.php
+++ index.php
@@ -1,1 +1,10 @@
-<?php include("index.html"); ?>
+<?php
+if(!isset($_SERVER["HTTPS"]) || $_SERVER["HTTPS"] != "on") {
+    //Tell the browser to redirect to the HTTPS URL.
+    header("Location: https://" . $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"], true, 301);
+    //Prevent the rest of the script from executing.
+    exit;
+} else {
+  include("index.html");
+}
+?>

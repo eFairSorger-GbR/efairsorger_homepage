250cf462857b6a3469c359266d2b17d8c82ed8ea
Kevin Liebholz <liebholz@crowddesk.de>
2020-12-20 12:03:07 +0100

    ssl

--- index.php
+++ index.php
@@ -1,1 +1,13 @@
-<?php ?>
+<?php
+  function Redirect($url, $permanent = false)
+  {
+      if (headers_sent() === false)
+      {
+          header('Location: ' . $url, true, ($permanent === true) ? 301 : 302);
+      }
+
+      exit();
+  }
+
+  Redirect('http://www.efairsorger.de/index.html', true);
+?>

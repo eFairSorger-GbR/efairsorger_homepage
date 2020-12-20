93090b4e0dc1c1e71c61225a76aca0e3ed19d6cf
Kevin Liebholz <liebholz@crowddesk.de>
2020-12-20 12:41:46 +0100

    add html again

--- index.php
+++ index.php
@@ -4,5 +4,7 @@ if(!isset($_SERVER["HTTPS"]) || $_SERVER["HTTPS"] != "on") {
     header("Location: https://" . $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"], true, 301);
     //Prevent the rest of the script from executing.
     exit;
+} else {
+  include("index.html");
 }
 ?>
